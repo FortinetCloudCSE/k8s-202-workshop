bbfa9750eca86c4c7695cf26ef38011694d803f1
Andy Wang <yagosys@gmail.com>
2024-06-06 15:40:32 +0800

    init

---
+++ scripts/labEnv/install_demo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+./01_Create_Azure_VM_New_VNET.sh && 
+./02_install_ingress_controller.sh && 
+./03_create_nodeportsvc.sh  
+sleep 5
+./04_create_ingressYaml.sh && 
+./05_updateNIC2ip.sh  &&
+./06_verify.sh
+if [ $? -ne 0 ]; then
+  echo "something failed"
+  exit 1
+fi

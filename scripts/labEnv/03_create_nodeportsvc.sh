c02429e2097401fa23e94b9aa3e8bba39fa0becd
Andy Wang <yagosys@gmail.com>
2024-06-07 07:18:47 +0800

    change to use geminiclientpod

--- scripts/labEnv/03_create_nodeportsvc.sh
+++ scripts/labEnv/03_create_nodeportsvc.sh
@@ -1,27 +1,6 @@
 #!/bin/bash -x
 
 cat << EOF | tee > siseNodePortSvc.yaml
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: sise
-  labels:
-    app: sise
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: sise
-  template:
-    metadata:
-      labels:
-        app: sise
-    spec:
-      containers:
-      - name: sise
-        image: mhausenblas/simpleservice:0.5.0
-        ports:
-        - containerPort: 9876
 ---
 kind: Service
 apiVersion: v1
@@ -40,7 +19,9 @@ spec:
     app: sise
   sessionAffinity: None
 EOF
+kubectl apply -f demogeminiclient.yaml
+kubectl rollout status deployment sise
+
 kubectl apply -f siseNodePortSvc.yaml
 
-kubectl rollout status deployment sise
 kubectl get ep service1

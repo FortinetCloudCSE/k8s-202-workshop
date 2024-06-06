d55a34e0a6cb73c99c28235bde46bff610959242
Andy Wang <yagosys@gmail.com>
2024-06-07 07:18:28 +0800

    init

--- scripts/labEnv/02_install_ingress_controller.sh
+++ scripts/labEnv/02_install_ingress_controller.sh
@@ -5,4 +5,4 @@ helm repo add FortiWeb-ingress-controller https://fortinet.github.io/fortiweb-in
 helm repo update
 kubectl create namespace $fortiwebingresscontrollernamespace
 helm install first-release  $releasename --namespace $fortiwebingresscontrollernamespace
-kubectl get pod -n $fortiwebingresscontrollernamespace
+kubectl rollout status deployment first-release-fwb-k8s-ctrl -n fortiwebingress

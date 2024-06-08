083b0da376f206b66e5a876f5b8d51aa2713bda8
Andy Wang <yagosys@gmail.com>
2024-06-08 12:39:01 +0800

    init

--- scripts/labEnv/sshfortiweb.sh
+++ scripts/labEnv/sshfortiweb.sh
@@ -1,3 +1,4 @@
+rsakeyname="id_rsa_tecworkshop"
 location="westus"
 fortiwebvmdnslabel="$(whoami)fortiwebvm7"
 echo $fortiwebvmdnslabel
@@ -5,4 +6,4 @@ vm_name="$fortiwebvmdnslabel.$location.cloudapp.azure.com"
 echo vm_name=$vm_name
 
 ssh-keygen -f "${HOME}/.ssh/known_hosts" -R "${vm_name}"
-ssh -o "StrictHostKeyChecking=no" azureuser@$vm_name 
+ssh -o "StrictHostKeyChecking=no" azureuser@$vm_name  -i  ~/.ssh/$rsakeyname

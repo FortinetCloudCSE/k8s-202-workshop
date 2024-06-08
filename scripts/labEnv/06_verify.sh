77ce3a77e491f9780e1826e4591e7aab45d34a2e
Andy Wang <yagosys@gmail.com>
2024-06-08 11:46:20 +0800

    add show aks outbound ip address

--- scripts/labEnv/06_verify.sh
+++ scripts/labEnv/06_verify.sh
@@ -2,9 +2,14 @@
 
 location="westus"
 fortiwebvmdnslabel="$(whoami)fortiwebvm7"
+owner="tecworkshop"
+aksClusterName=$(whoami)-aks-cluster
 echo $fortiwebvmdnslabel
 vm_name="$fortiwebvmdnslabel.$location.cloudapp.azure.com"
 svcdnsname="$(whoami)px2.$location.cloudapp.azure.com"
+resourceGroupName=$owner-$(whoami)-"fortiweb-"$location-$(date -I)
+remoteResourceGroup="MC"_${resourceGroupName}_${aksClusterName}_${location} 
+
 #svcdnsname=${fortiwebvmdnslabel}port2.westus.cloudapp.azure.com 
 #svcdnsname="test.com"
 
@@ -54,8 +59,12 @@ sleep 2
 
 echo "Checking pod to ingress via FortiWeb"
 
+podegressip=$(az network public-ip list --resource-group $remoteResourceGroup  | jq -r .[0].ipAddress)
+echo $podegressip 
+echo below will test gemini api , make sure you have input the gemini API token as well as allow $podegressip has access to gemini API 
+
 # Execute curl command with Host header inside the clientpod pod
-kubectl exec -it po/clientpod -- curl -v -H "Host: $svcdnsname" http://10.0.2.100:80/generate | grep "HTTP/1.1 200 OK"
+kubectl exec -it po/clientpod -- curl -v -H "Host: $svcdnsname" http://10.0.2.100:80/generate  -H "Content-Type: application/json" -d '{"prompt": "hi"}' | grep "HTTP/1.1 200 OK"
 if [ $? -ne 0 ]; then
   echo "Failed to curl ingress via FortiWeb"
   exit 1
@@ -66,7 +75,7 @@ echo verify is succcessful
 
 echo "sending malicious traffic" 
 
-kubectl exec -it po/clientpod -- curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d  '%{(#_='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).(#_memberAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.ActionContext.container']).(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class)).(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear()).(#context.setMemberAccess(#dm)))).(#cmd='bash').(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win'))).(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd})).(#p=new java.lang.ProcessBuilder(#cmds)).(#p.redirectErrorStream(true)).(#process=#p.start()).(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream())).(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros)).(#ros.flush())}' -H "Host: $svcdnsname" http://10.0.2.100:80/generate | grep "been blocked"
+kubectl exec -it po/clientpod -- curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d  '%{(#_='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).(#_memberAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.ActionContext.container']).(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class)).(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear()).(#context.setMemberAccess(#dm)))).(#cmd='bash').(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win'))).(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd})).(#p=new java.lang.ProcessBuilder(#cmds)).(#p.redirectErrorStream(true)).(#process=#p.start()).(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream())).(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros)).(#ros.flush())}' -H "Host: $svcdnsname" http://10.0.2.100:80/generate -H "Content-Type: application/json" -d '{"prompt": "hi"}'| grep "been blocked"
 
 if [ $? -ne 0 ]; then
   echo "Failed to curl ingress via FortiWeb"
@@ -78,4 +87,9 @@ echo traffic blocked by fortiweb sucessfully
 
 #kubectl exec -it po/clientpod -- curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d  '%{(#_='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).(#_memberAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.ActionContext.container']).(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class)).(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear()).(#context.setMemberAccess(#dm)))).(#cmd='bash').(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win'))).(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd})).(#p=new java.lang.ProcessBuilder(#cmds)).(#p.redirectErrorStream(true)).(#process=#p.start()).(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream())).(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros)).(#ros.flush())}' -H "Host: $svcdnsname" http://curl -v $svcdnsname:80/generate | grep "been blocked"
 
+podegressip=$(az network public-ip list --resource-group $remoteResourceGroup  | jq -r .[0].ipAddress)
+echo $podegressip
+
+
+curl http://ipx2.westus.cloudapp.azure.com/generate -H "Content-Type: application/json" -d '{"prompt": "your prompt here"}' 
 echo "All checks completed successfully"
